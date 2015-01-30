f960ceaad4208c537c8d5229d1b756c7107574cb
Chris Leet <christopher.leet@yale.edu>
2015-01-29 22:02:17 -0500

    Website stuff

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	// Grid selection
+	var grid = $("#grid-container");
+
+	// // Populate grid container with grid cells
+	// for ( var i = 0; i < grid.cellCount; i++ ) {
+	// 	grid.append('<div class="cell" id="' + i + '"></div>');
+	// 	grid.cells[i] = $("#" + i);
+	// }
+
+});
